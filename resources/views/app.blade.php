6f4cc5c174b00f3bb4fa9f174feebb21710ec2ec
Maksym Holovchenko <topcoder.premiumware@gmail.com>
2022-05-10 11:36:53 +0300

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1, width=device-width"/>
+        <title>{{ env('APP_NAME') }}</title>
+
+    </head>
+
+    <body>
+        <div id="app"></div>
+        <script src="{{asset('js/app.js')}}"></script>
+    </body>
+
+</html>

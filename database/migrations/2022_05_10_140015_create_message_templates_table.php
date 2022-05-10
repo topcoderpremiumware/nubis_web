e19c910a59179c3e7cfdb25053731bb9323c8264
Maksym Holovchenko <topcoder.premiumware@gmail.com>
2022-05-10 18:09:41 +0300

    Added database migrations

---
+++ database/migrations/2022_05_10_140015_create_message_templates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_templates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('place_id')->constrained()->onDelete('cascade');
+            $table->string('purpose');
+            $table->string('subject');
+            $table->text('text');
+            $table->string('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message_templates');
+    }
+};
---
+++ database/migrations/2022_05_10_140020_create_feedbacks_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('place_id')->constrained()->onDelete('cascade');
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->text('comment');
+            $table->string('status');
+            $table->double('food_mark',3,1);
+            $table->double('service_mark',3,1);
+            $table->double('ambiance_mark',3,1);
+            $table->double('experience_mark',3,1);
+            $table->double('price_mark',3,1);
+            $table->double('average_mark',3,1);
+            $table->binary('is_recommend');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+};

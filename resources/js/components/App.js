3b57dabdd0ea8bf04af53549a74330f908332dea
Top <topcoder.premiumware@gmail.com>
2022-05-27 23:11:51 +0300

    change style home

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -14,7 +14,7 @@ function App() {
     return (
         <BrowserRouter>
         <Topbar/>
-        <div className="container">
+        <div className="content">
             <Sidebar/>
             {/* <Routes>
                 <Route path='/' exact element={<DailyUse/>} />

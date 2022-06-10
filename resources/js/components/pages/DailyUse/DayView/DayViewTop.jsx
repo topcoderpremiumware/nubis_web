f03282a4a5262281189b46a5f320829378f2b52a
Top <topcoder.premiumware@gmail.com>
2022-06-10 16:00:35 +0300

    DailyViewSelect

--- resources/js/components/pages/DailyUse/DayView/DayViewTop.jsx
+++ resources/js/components/pages/DailyUse/DayView/DayViewTop.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 
-export default function DayViewTop() {
-  return (
-    <h2>DayViewTop buttons</h2>
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
+
+export default function DayViewTop() {
+
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div className='DayViewSelect__container'>
+      <FormControl sx={{ minWidth: 120 }} className='DayViewSelect'>
+        <Select
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem value="">
+            <em>Select</em>
+          </MenuItem>
+          <MenuItem value={10}>Select 1</MenuItem>
+          <MenuItem value={20}>Select 2</MenuItem>
+          <MenuItem value={30}>Select 3</MenuItem>
+        </Select>
+        {/* <FormHelperText>Without label</FormHelperText> */}
+      </FormControl>
+    </div>
   )
 }
-
-

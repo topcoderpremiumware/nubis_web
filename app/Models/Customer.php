8e6b384f0315fe5a70ef1c0e605a9709272b15a2
Maksym Holovchenko <topcoder.premiumware@gmail.com>
2022-06-15 23:06:33 +0300

    Fixed customer auth

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
-    use HasApiTokens, HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $guarded = [];
 

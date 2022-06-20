f39619aa1016944bad3b1566ad7101c26b368b9e
Top <topcoder.premiumware@gmail.com>
2022-06-20 22:52:05 +0300

    select style

--- resources/js/components/pages/DailyUse/DayView/DayViewTop.jsx
+++ resources/js/components/pages/DailyUse/DayView/DayViewTop.jsx
@@ -1,36 +1,15 @@
-import React from 'react'
-
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import React from 'react';
+import PointSelect from './select/point-select';
+import TimeSelect from './select/time-select';
+import  './DayViewTop.scss';
 
 
 export default function DayViewTop() {
 
-  const [age, setAge] = React.useState('');
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
   return (
-    <div className='DayViewSelect__container'>
-      <FormControl sx={{ minWidth: 120 }} className='DayViewSelect'>
-        <Select
-          value={age}
-          onChange={handleChange}
-          displayEmpty
-          inputProps={{ 'aria-label': 'Without label' }}
-        >
-          <MenuItem value="">
-            <em>Select</em>
-          </MenuItem>
-          <MenuItem value={10}>Select 1</MenuItem>
-          <MenuItem value={20}>Select 2</MenuItem>
-          <MenuItem value={30}>Select 3</MenuItem>
-        </Select>
-        {/* <FormHelperText>Without label</FormHelperText> */}
-      </FormControl>
+    <div className='DayViewTop__container'>
+      <PointSelect/>
+      <TimeSelect/>
     </div>
   )
 }

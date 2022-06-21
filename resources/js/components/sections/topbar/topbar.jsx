47f6d0d8fc7d9c5e4e8fa3ed1e554070ca79335d
Maksym Holovchenko <topcoder.premiumware@gmail.com>
2022-06-21 14:31:12 +0300

    Commit for merge images

--- resources/js/components/sections/topbar/topbar.jsx
+++ resources/js/components/sections/topbar/topbar.jsx
@@ -27,7 +27,7 @@ export default function Topbar() {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark sticky-top">
         <div className="container">
-          <Link className="navbar-brand" to="/">DinnerBooking</Link>
+          <Link className="navbar-brand" to="/">OnlineDinnerBooker</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                   aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"/>

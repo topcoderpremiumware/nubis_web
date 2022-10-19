2dcade782d9ab1ca2e58a550460b061ff8c013bb
Louie787 <tigrovakyla11@gmail.com>
2022-10-19 17:37:34 +0300

    customers table

--- resources/js/components/pages/DayView/bottompanel/newbooking/tabs/TabNewBooking/tables/GuestTablesApi.jsx
+++ resources/js/components/pages/DayView/bottompanel/newbooking/tabs/TabNewBooking/tables/GuestTablesApi.jsx
@@ -1,50 +1,51 @@
-
 import React, {Component }from 'react';
-// import './../DataTables.css';
-// import './CustomDataTablesBooking.scss';
+import './GuestTables.scss'
 
 const $ = require('jquery');
 $.DataTable = require('datatables.net');
 
 class Tables extends Component {
-    componentDidMount(){
-        // console.log(this.el)
-        this.el = $(this.el)
-        this.el.DataTable(
-           {
-            retrieve: true,
-            paging: false,
-            scrollY: 120,
-            scrollX: 120,
-            autoWidth: true,
-               data: this.props.data,
-               columns: [
-                {title: "First Name"}, 
-                {title: "Last Name"}, 
-                {title: "Company"}, 
-                {title: "Address"}, 
-                {title: "Phone"}, 
-                {title: "Email"}, 
-                {title: "Room no."}, 
-                {title: "Member no."}, 
-               ]
-           } 
-        )
-
-    }
-    componentWillUnmount(){
-        // this.$el.DataTable.destroy(true);
-    }
-
-    render(){
-      return(
-        <div>
-          <table class="display" width="100%" ref={el => this.el = el}> 
-
-          </table>
-        </div>
-      );
-    }
+  componentDidMount() {
+    this.el = $(this.el)
+    this.el.DataTable({
+      retrieve: true,
+      paging: false,
+      scrollY: 180,
+      autoWidth: true
+    })
   }
+
+  render() {
+    return(
+      <div className="guest-table">
+        <table width="100%" ref={el => this.el = el}>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Zip Code</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.data.map(i => (
+              <tr
+                key={i.id}
+                onClick={() => this.props.onClick(i)}
+              >
+                <td>{i.first_name || '-'}</td>
+                <td>{i.last_name || '-'}</td>
+                <td>{i.email || '-'}</td>
+                <td>{i.phone || '-'}</td>
+                <td>{i.zip_code || '-'}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default Tables;

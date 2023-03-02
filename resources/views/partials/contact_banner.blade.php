412025d2f0ea3fc4d79aac9734a82d9fe2438cd9
Louie787 <tigrovakyla11@gmail.com>
2023-03-02 18:53:37 +0200

    contact page

---
+++ resources/views/partials/contact_banner.blade.php
@@ -0,0 +1,10 @@
+<section class="banner contact-banner">
+  <div class="container">
+    <h2 class="title banner-title">{{__('Subscribe to newsletter in Nubis!')}}</h2>
+    <p class="banner-text">{{__('Subscribing to Nubis You agree to our Privacy and Terms')}}</p>
+    <form action="#" class="contact-banner-form">
+      <input type="email" required class="contact-banner-input" placeholder="{{__('Email')}}">
+      <button type="submit" class="contact-banner-btn">{{__('Subscribe')}}</button>
+    </form>
+  </div>
+</section>

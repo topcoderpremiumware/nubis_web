1fea5ec01ddb93b726cdac2bd5da125fbc4d9eb3
Maksym Holovchenko <2ovob4ehko@gmail.com>
2023-04-13 17:23:37 +0300

    Fixed language switcher

--- resources/js/i18nextConf.js
+++ resources/js/i18nextConf.js
@@ -3,8 +3,9 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+
 const fallbackLng = 'en';
-const availableLanguages = ['en', 'da'];
+const availableLanguages = window.langs.map(item => item.lang)
 
 i18n
     .use(Backend) // load translations using http (default                                               public/assets/locals/en/translations)

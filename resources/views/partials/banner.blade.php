0ccb938b8902af99c6e2cf761e3e07068ee1666f
Louie787 <tigrovakyla11@gmail.com>
2023-05-04 12:05:24 +0300

    added phone

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -3,5 +3,6 @@
     <h2 class="title banner-title">{{__('Free creation and only 20€ per month*')}}</h2>
     <p class="banner-text">{{__('The first month is always free and without obligation. We can also help with the setup, implementation on the website, and staff training.')}}</p>
     <a href="/pricing" class="banner-link">{{__('See our prices')}}</a>
+    <p class="banner-text">{{__('Customer service phone')}} <a href="tel:+4531905250" class="banner-link">+45 31905250</a></p>
   </div>
 </section>

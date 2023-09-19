31d9f7a6d15271fa3e7f2eae6bfdbc4aec5ba5fe
Maksym Holovchenko <2ovob4ehko@gmail.com>
2023-09-19 16:03:07 +0300

    added google Tag Manager

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,20 @@
     <link rel="stylesheet" href="/css/home.css">
     <title>{{ env('APP_NAME') }}</title>
     @include('partials/meta')
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f)
+      ;})(window,document,'script','dataLayer','GTM-58X6C4W9');</script>
+  <!-- End Google Tag Manager -->
   </head>
 
   <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58X6C4W9
+"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
     @include('partials/header')
 
     <main class="main">

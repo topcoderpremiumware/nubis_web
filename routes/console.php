fbbbfe3d925dd50f2ba66f1b465e63986311f552
Maksym Holovchenko <2ovob4ehko@gmail.com>
2023-09-21 18:27:13 +0300

    fixed timezone bug

--- routes/console.php
+++ routes/console.php
@@ -19,32 +19,32 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Artisan::command('time_migration', function () {
-    $places = \App\Models\Place::all();
-    foreach ($places as $place) {
-        foreach ($place->custom_booking_lengths as $item) {
-            $new_interval = [];
-            foreach ($item->time_intervals as $time_interval) {
-                $new_interval[] = [
-                    'from' => \Carbon\Carbon::parse($time_interval['from'])
-                        ->timezone($place->country->timezone)->format('H:i:s'),
-                    'to' => \Carbon\Carbon::parse($time_interval['to'])
-                        ->timezone($place->country->timezone)->format('H:i:s')
-                ];
-            }
-            $item->time_intervals = $new_interval;
-            $item->save();
-        }
-        foreach ($place->orders as $order) {
-            $order->reservation_time = \Carbon\Carbon::parse($order->reservation_time)
-                ->timezone($place->country->timezone)->format('Y-m-d H:i:s');
-            $order->save();
-        }
-        foreach ($place->timetables as $timetable) {
-            $timetable->start_time = \Carbon\Carbon::parse($timetable->start_time)
-                ->timezone($place->country->timezone)->format('Y-m-d H:i:s');
-            $timetable->end_time = \Carbon\Carbon::parse($timetable->end_time)
-                ->timezone($place->country->timezone)->format('Y-m-d H:i:s');
-            $timetable->save();
-        }
-    }
+//    $places = \App\Models\Place::all();
+//    foreach ($places as $place) {
+//        foreach ($place->custom_booking_lengths as $item) {
+//            $new_interval = [];
+//            foreach ($item->time_intervals as $time_interval) {
+//                $new_interval[] = [
+//                    'from' => \Carbon\Carbon::parse($time_interval['from'])
+//                        ->timezone($place->country->timezone)->format('H:i:s'),
+//                    'to' => \Carbon\Carbon::parse($time_interval['to'])
+//                        ->timezone($place->country->timezone)->format('H:i:s')
+//                ];
+//            }
+//            $item->time_intervals = $new_interval;
+//            $item->save();
+//        }
+//        foreach ($place->orders as $order) {
+//            $order->reservation_time = \Carbon\Carbon::parse($order->reservation_time)
+//                ->timezone($place->country->timezone)->format('Y-m-d H:i:s');
+//            $order->save();
+//        }
+//        foreach ($place->timetables as $timetable) {
+//            $timetable->start_time = \Carbon\Carbon::parse($timetable->start_time)
+//                ->timezone($place->country->timezone)->format('Y-m-d H:i:s');
+//            $timetable->end_time = \Carbon\Carbon::parse($timetable->end_time)
+//                ->timezone($place->country->timezone)->format('Y-m-d H:i:s');
+//            $timetable->save();
+//        }
+//    }
 })->purpose('Migrate time');

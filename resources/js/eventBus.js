96402eb5b8424febbe77b91a599e5af7472393ea
Maksym Holovchenko <2ovob4ehko@gmail.com>
2024-07-23 10:36:07 +0300

    fixed dayview useEffect

--- resources/js/eventBus.js
+++ resources/js/eventBus.js
@@ -1,12 +1,27 @@
 const eventBus = {
+  listeners: {},
+
   on(event, callback) {
-    document.addEventListener(event, (e) => callback(e.detail));
+    const wrappedCallback = (e) => callback(e.detail);
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push({ original: callback, wrapped: wrappedCallback });
+    document.addEventListener(event, wrappedCallback);
   },
+
   dispatch(event, data) {
     document.dispatchEvent(new CustomEvent(event, { detail: data }));
   },
+
   remove(event, callback) {
-    document.removeEventListener(event, callback);
+    if (this.listeners[event]) {
+      const listenerObj = this.listeners[event].find(listener => listener.original === callback);
+      if (listenerObj) {
+        document.removeEventListener(event, listenerObj.wrapped);
+        this.listeners[event] = this.listeners[event].filter(listener => listener.original !== callback);
+      }
+    }
   },
 };
 

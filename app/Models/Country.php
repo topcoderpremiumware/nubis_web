7de151a92bf70a8834ad9ff9c6cf93b424957ea9
Maksym Holovchenko <2ovob4ehko@gmail.com>
2024-10-22 15:56:18 +0300

    Added country code

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -6,6 +6,13 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property integer $id
+ * @property string $name
+ * @property string $timezone
+ * @property string $code
+ *
+ */
 class Country extends Model
 {
     use HasFactory;

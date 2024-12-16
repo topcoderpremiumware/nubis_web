efed694cb7b3f799a575f8eb262ecd2e70cacce8
Maksym Holovchenko <2ovob4ehko@gmail.com>
2024-12-16 16:26:00 +0200

    Added QZ keys

--- resources/js/components/pages/Receipts/QZSettings.jsx
+++ resources/js/components/pages/Receipts/QZSettings.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {useTranslation} from "react-i18next";
 import {Button, Grid, Stack} from "@mui/material";
 import eventBus from "../../../eventBus";
+import Box from "@mui/material/Box";
 
 export default function QZSettings() {
   const { t } = useTranslation();
@@ -57,6 +58,16 @@ export default function QZSettings() {
         </Grid>
       </Grid>
       <Button variant="contained" onClick={onSave}>{t('Save')}</Button>
+      <Box sx={{mt:3}}>
+        <p>Instruction:
+          <ol>
+            <li>Go to the <a href="https://qz.io/download/">https://qz.io/download/</a>, download app and install it.</li>
+            <li>A printer icon will appear next to the date and time on the main menu bar of the operating system. Click on it in the menu and select Advanced -> Site manager...</li>
+            <li>In the open window, press the blue "+" button, then "Create new". Then agree to all the questions. After the questions, a folder with two files will open. The folder is on the desktop. There are two files in this folder: .txt is the certificate and .pem is the key.</li>
+            <li>These files must be uploaded to the form above.</li>
+          </ol>
+        </p>
+      </Box>
     </div>
   )
 }

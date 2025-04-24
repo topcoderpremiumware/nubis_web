ff4f34209dbcf1b6ac25bf0a1faf9d6426412c43
Maksym Holovchenko <2ovob4ehko@gmail.com>
2025-04-24 10:55:04 +0300

    Added all places for superadmin

--- resources/js/components/sections/sidebar/SidebarData.jsx
+++ resources/js/components/sections/sidebar/SidebarData.jsx
@@ -215,7 +215,7 @@ export const SidebarData = (data) => { return [
       {
         title: 'POS',
         path: '/POS',
-        show: isBills(['pos','pos_terminal']) && !isBills(['booking','full']),
+        show: data.is_superadmin || (isBills(['pos','pos_terminal']) && !isBills(['booking','full'])),
       },
       {
         title: 'Receipts',
